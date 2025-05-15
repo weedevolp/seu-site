968695fc5be130c2d091a8093758c904412d1a91
WeeDevelop <backdoorcortes@gmail.com>
2025-05-15 11:15:45 -0300

    Update smoothscoll.tsx

--- src/components/smoothscoll.tsx
+++ src/components/smoothscoll.tsx
@@ -5,19 +5,23 @@ import { useEffect } from "react"
 export default function SmoothScroll() {
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
-      const target = e.target as HTMLAnchorElement;
-      if (target.tagName === 'A' && target.hash) {
-        e.preventDefault();
-        const element = document.querySelector(target.hash);
+      const target = e.target as HTMLElement
+      if (target.tagName === "A" && target.getAttribute("href")?.startsWith("#")) {
+        e.preventDefault()
+        const id = target.getAttribute("href")?.slice(1)
+        const element = document.getElementById(id!)
         if (element) {
-          element.scrollIntoView({ behavior: 'smooth' });
+          element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          })
         }
       }
-    };
+    }
 
-    document.addEventListener('click', handleClick);
-    return () => document.removeEventListener('click', handleClick);
-  }, []);
+    document.addEventListener("click", handleClick)
+    return () => document.removeEventListener("click", handleClick)
+  }, [])
 
-  return null;
+  return null
 }
